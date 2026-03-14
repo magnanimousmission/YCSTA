614929b406a18a79ea9c296bf948adb9d8ca09d0
magnanimousmission <mikeasotelojr@gmail.com>
2026-03-13 23:23:05 -0400

    Implemented nav nesh and built out the scene
    Navmesh now controls AI when you walk up and interact they will follow you as long as you stay in their trigger collider
    
    First point of interest complete (Guard house)

--- Assets/_Project/Scripts/NPCs/NPCCore.cs
+++ Assets/_Project/Scripts/NPCs/NPCCore.cs
@@ -5,6 +5,7 @@ using Assets._Project.Scripts.Player.States;
 using Assets.Scripts.Player;
 using Unity.VisualScripting;
 using Assets.Scripts.NPCs;
+using UnityEngine.AI;
 
 public class NPCCore : MonoBehaviour
 {
@@ -15,7 +16,7 @@ public class NPCCore : MonoBehaviour
     [SerializeField] private float npcHealth;
     [SerializeField] private float npcEnergy;
     [SerializeField] private float npcOxygen;
-
+    [SerializeField] private NavMeshAgent agent;
     [SerializeField] bool teamMember = false;
 
     internal void SetJumpCooldown(float duration)
@@ -106,6 +107,11 @@ public class NPCCore : MonoBehaviour
         input.npcInput += NPCInputHandler_playerInput;
         Cursor.visible = false;
         initialized = true;
+    }
+
+    internal NavMeshAgent GetAgent()
+    {
+        return agent;
     }
 
     private void OnDestroy()
